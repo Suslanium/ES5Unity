1c226280b4de4f0d9b3c86ad48214e72d96a9174
Suslanium <zaharov22m@gmail.com>
2023-07-19 22:49:10 +0700

    Add partial CELL record support

---
+++ Assets/Scripts/MasterFile/MasterFileContents/Records/CELL.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using Ionic.Zlib;
+using MasterFile.MasterFileContents.Records.Structures;
+using UnityEngine;
+
+namespace MasterFile.MasterFileContents.Records
+{
+    /// <summary>
+    /// <para>CELL records contain the data for either interior (CELL top group) or exterior (WRLD top group) cells. They follow the same format in both cases. The CELL record is followed by a group containing the references for the cell, organized into subgroups. Persistent references are defined in the 'persistent children' subgroup while temporary references are defined in the 'temporary children' subgroup. The 'persistent children' subgroup must occur before the 'temporary children' subgroup.</para>
+    /// <para>The block and sub-block groups for an interior cell can be determined either by the label field of the group headers of the block and sub-block, or by the last two decimal digits of the Form ID. For example, for Form ID 0x138CA, which is 80,074 in decimal, 4 indicates that this is block 4, and the 7 indicates that it's sub-block 7.</para>
+    /// <para>The block and sub-block groups for an exterior cell can be determined either by the label filed of the group headers of the block and sub-block, or by the X, Y coordinates of the cell from the XCLC field. Each block contains 16 sub-blocks (4x4) and each sub-block contains 64 cells (8x8). So, to get the sub-block number, divide the coordinates by 8 (rounding down), and the block number can be determined by dividing the sub-block numbers by 4 (again, rounding down). Thus, a cell with the X, Y coordinates 31, 50 would be in floor(31 / 8) = 3, floor(50 / 8) = 6, so sub-block 3, 6. Dividing again, only this time by 4, it would be in block 0, 1.</para>
+    /// </summary>
+    public class CELL : Record
+    {
+        public string EditorID { get; private set; }
+
+        public uint LocalizedNameID { get; private set; }
+
+        /// <summary>
+        /// <para>flags - Sometimes the field is only one byte long</para>
+        /// <para>0x0001 - Interior</para>
+        /// <para>0x0002 - Has Water</para>
+        /// <para>0x0004 - not Can't Travel From Here - only valid for interior cells</para>
+        /// <para>0x0008 - No LOD Water</para>
+        /// <para>0x0020 - Public Area</para>
+        /// <para>0x0040 - Hand Changed</para>
+        /// <para>0x0080 - Show Sky</para>
+        /// <para>0x0100 - Use Sky Lighting</para>
+        /// </summary>
+        public ushort CellFlag { get; private set; }
+
+        /// <summary>
+        /// Always in exterior cells and never in interior cells.
+        /// </summary>
+        public int XGridPosition { get; private set; }
+
+        /// <summary>
+        /// Always in exterior cells and never in interior cells.
+        /// </summary>
+        public int YGridPosition { get; private set; }
+
+        public Lighting CellLightingInfo { get; private set; }
+
+        /// <summary>
+        /// The lighting template for this cell.
+        /// </summary>
+        public uint LightingTemplateReference { get; private set; }
+
+        /// <summary>
+        /// <para>Non-ocean water-height in cell, is used for rivers, ponds etc., ocean-water is globally defined elsewhere.</para>
+        /// <para>0x7F7FFFFF reserved as ID for "no water present", it is also the maximum positive float.</para>
+        /// <para>0x4F7FFFC9 is a bug in the CK, this is the maximum unsigned integer 2^32-1 cast to a float and means the same as above</para>
+        /// <para>0xCF000000 could be a bug as well, this is the maximum signed negative integer -2^31 cast to a float</para>
+        /// </summary>
+        public float NonOceanWaterHeight { get; private set; }
+
+        /// <summary>
+        /// The location for (of?) this cell.
+        /// </summary>
+        public uint LocationReference { get; private set; }
+
+        /// <summary>
+        /// The water for (of?) this cell.
+        /// </summary>
+        public uint WaterReference { get; private set; }
+
+        /// <summary>
+        /// Water Environment Map (only interior cells)
+        /// </summary>
+        public string WaterEnvironmentMap { get; private set; }
+
+        /// <summary>
+        /// The acoustic space for this cell.
+        /// </summary>
+        public uint AcousticSpaceReference { get; private set; }
+
+        /// <summary>
+        /// The music type for this cell.
+        /// </summary>
+        public uint MusicTypeReference { get; private set; }
+
+        /// <summary>
+        /// The image space for this cell.
+        /// </summary>
+        public uint ImageSpaceReference { get; private set; }
+
+        private CELL(string type, uint dataSize, uint flag, uint formID, ushort timestamp, ushort versionControlInfo,
+            ushort internalRecordVersion, ushort unknownData) : base(type, dataSize, flag, formID, timestamp,
+            versionControlInfo, internalRecordVersion, unknownData)
+        {
+        }
+
+        public static CELL ParseSpecific(Record baseInfo, BinaryReader fileReader, long position)
+        {
+            CELL cell = new CELL(baseInfo.Type, baseInfo.DataSize, baseInfo.Flag, baseInfo.FormID, baseInfo.Timestamp,
+                baseInfo.VersionControlInfo, baseInfo.InternalRecordVersion, baseInfo.UnknownData);
+            while (fileReader.BaseStream.Position < position + baseInfo.DataSize)
+            {
+                string fieldType = new string(fileReader.ReadChars(4));
+                ushort fieldSize = fileReader.ReadUInt16();
+                switch (fieldType)
+                {
+                    case "EDID":
+                        cell.EditorID = new string(fileReader.ReadChars(fieldSize));
+                        break;
+                    case "FULL":
+                        cell.LocalizedNameID = fileReader.ReadUInt32();
+                        break;
+                    case "DATA":
+                        cell.CellFlag = fieldSize == 1 ? fileReader.ReadByte() : fileReader.ReadUInt16();
+                        break;
+                    case "XCLC":
+                        cell.XGridPosition = fileReader.ReadInt32();
+                        cell.YGridPosition = fileReader.ReadInt32();
+                        fileReader.ReadUInt32();
+                        break;
+                    case "XCLL":
+                        cell.CellLightingInfo = Lighting.parseFromCELL(fieldSize, fileReader);
+                        break;
+                    case "LTMP":
+                        cell.LightingTemplateReference = fileReader.ReadUInt32();
+                        break;
+                    case "XCLW":
+                        cell.NonOceanWaterHeight = fileReader.ReadSingle();
+                        break;
+                    case "XLCN":
+                        cell.LocationReference = fileReader.ReadUInt32();
+                        break;
+                    case "XCWT":
+                        cell.WaterReference = fileReader.ReadUInt32();
+                        break;
+                    case "XWEM":
+                        cell.WaterEnvironmentMap = new string(fileReader.ReadChars(fieldSize));
+                        break;
+                    case "XCAS":
+                        cell.AcousticSpaceReference = fileReader.ReadUInt32();
+                        break;
+                    case "XCMO":
+                        cell.MusicTypeReference = fileReader.ReadUInt32();
+                        break;
+                    case "XCIM":
+                        cell.ImageSpaceReference = fileReader.ReadUInt32();
+                        break;
+                    default:
+                        fileReader.BaseStream.Seek(fieldSize, SeekOrigin.Current);
+                        break;
+                }
+            }
+
+            return cell;
+        }
+    }
+}
---
+++ Assets/Scripts/MasterFile/MasterFileContents/Records/Structures/Lighting.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+
+namespace MasterFile.MasterFileContents.Records.Structures
+{
+    /// <summary>
+    /// Lighting information
+    /// </summary>
+    public class Lighting
+    {
+        public byte[] AmbientRGBA { get; private set; }
+        public byte[] DirectionalRGBA { get; private set; }
+        public int DirectionalRotationXY { get; private set; }
+        public int DirectionalRotationZ { get; private set; }
+        public float DirectionalFade { get; private set; }
+        public byte[] FogNearColor { get; private set; }
+        public float FogNear { get; private set; }
+        public float FogFar { get; private set; }
+        public byte[] FogFarColor { get; private set; }
+        public float FogMax { get; private set; }
+        public float LightFadeDistanceStart { get; private set; }
+        public float LightFadeDistanceEnd { get; private set; }
+
+        /// <summary>
+        /// Inherit flags - controls which parts are inherited from Lighting Template(only present in CELL records)
+        /// <para>0x0001 - Ambient Color</para>
+        /// <para>0x0002 - Directional Color</para>
+        /// <para>0x0004 - Fog Color</para>
+        /// <para>0x0008 - Fog Near</para>
+        /// <para>0x0010 - Fog Far</para>
+        /// <para>0x0020 - Directional Rot</para>
+        /// <para>0x0040 - Directional Fade</para>
+        /// <para>0x0080 - Clip Distance</para>
+        /// <para>0x0100 - Fog Power</para>
+        /// <para>0x0200 - Fog Max</para>
+        /// <para>0x0400 - Light Fade Distances</para>
+        /// </summary>
+        public uint InheritFlags { get; private set; }
+
+        private Lighting() {}
+        
+        /// <summary>
+        /// ONLY to be used inside ParseSpecific function of CELL Record
+        /// </summary>
+        public static Lighting parseFromCELL(ushort fieldSize, BinaryReader fileReader)
+        {
+            if (fieldSize == 92)
+            {
+                Lighting lighting = new Lighting
+                {
+                    AmbientRGBA = fileReader.ReadBytes(4),
+                    DirectionalRGBA = fileReader.ReadBytes(4),
+                    FogNearColor = fileReader.ReadBytes(4),
+                    FogNear = fileReader.ReadSingle(),
+                    FogFar = fileReader.ReadSingle(),
+                    DirectionalRotationXY = fileReader.ReadInt32(),
+                    DirectionalRotationZ = fileReader.ReadInt32(),
+                    DirectionalFade = fileReader.ReadSingle()
+                };
+                //Skip some unknown/unused info
+                fileReader.BaseStream.Seek(40, SeekOrigin.Current);
+                lighting.FogFarColor = fileReader.ReadBytes(4);
+                lighting.FogMax = fileReader.ReadSingle();
+                lighting.LightFadeDistanceStart = fileReader.ReadSingle();
+                lighting.LightFadeDistanceEnd = fileReader.ReadSingle();
+                lighting.InheritFlags = fileReader.ReadUInt32();
+                return lighting;
+            }
+            else
+            {
+                fileReader.BaseStream.Seek(fieldSize, SeekOrigin.Current);
+                return null;
+            }
+        }
+    }
+}
