0f75634d8cc871ed50e68774e4550a287eecf25d
Suslanium <zaharov22m@gmail.com>
2023-08-03 01:11:12 +0700

    Cell lighting import (WIP)

--- Assets/Scripts/Engine/CellManager.cs
+++ Assets/Scripts/Engine/CellManager.cs
@@ -7,6 +7,7 @@ using MasterFile.MasterFileContents;
 using MasterFile.MasterFileContents.Records;
 using NIF.Converter;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Engine
 {
@@ -47,6 +48,7 @@ namespace Engine
                 throw new InvalidDataException("Cell children group not found");
             var cellGameObject = new GameObject(editorID);
             var cellInfo = new CellInfo(cellGameObject, cell);
+            ConfigureCellLighting(cell);
             foreach (var subGroup in childrenGroup.GroupData)
             {
                 if (subGroup is not Group group) continue;
@@ -59,6 +61,118 @@ namespace Engine
             }
 
             _cells.Add(cellInfo);
+        }
+
+        private void ConfigureCellLighting(CELL cellRecord)
+        {
+            LGTM template = null;
+            if (cellRecord.LightingTemplateReference > 0)
+                template = (LGTM)_masterFile.GetFromFormID(cellRecord.LightingTemplateReference);
+            var directionalLight = RenderSettings.sun;
+            //Inherit ambient color
+            RenderSettings.ambientMode = AmbientMode.Flat;
+            if ((cellRecord.CellLightingInfo.InheritFlags & 0x0001) != 0 && template != null)
+            {
+                RenderSettings.ambientLight = new Color32(template.LightingData.AmbientRGBA[0],
+                    template.LightingData.AmbientRGBA[1], template.LightingData.AmbientRGBA[2], 255);
+            }
+            else
+            {
+                RenderSettings.ambientLight = new Color32(cellRecord.CellLightingInfo.AmbientRGBA[0],
+                    cellRecord.CellLightingInfo.AmbientRGBA[1], cellRecord.CellLightingInfo.AmbientRGBA[2], 255);
+            }
+
+            //Inherit directional color
+            if ((cellRecord.CellLightingInfo.InheritFlags & 0x0002) != 0 && template != null)
+            {
+                var directionalColor = new Color32(template.LightingData.DirectionalRGBA[0],
+                    template.LightingData.DirectionalRGBA[1], template.LightingData.DirectionalRGBA[2], 255);
+                if (directionalColor != Color.black)
+                {
+                    directionalLight.enabled = true;
+                    directionalLight.color = directionalColor;
+                    var rotation = Quaternion.identity;
+                    rotation *= NifUtils.NifEulerAnglesToUnityQuaternion(
+                        new Vector3(template.LightingData.DirectionalRotationXY,
+                            template.LightingData.DirectionalRotationXY, template.LightingData.DirectionalRotationZ));
+                    directionalLight.transform.rotation = rotation;
+                }
+                else
+                {
+                    directionalLight.enabled = false;
+                }
+            }
+            else
+            {
+                var directionalColor = new Color32(cellRecord.CellLightingInfo.DirectionalRGBA[0],
+                    cellRecord.CellLightingInfo.DirectionalRGBA[1], cellRecord.CellLightingInfo.DirectionalRGBA[2],
+                    255);
+                if (directionalColor != Color.black)
+                {
+                    directionalLight.enabled = true;
+                    directionalLight.color = directionalColor;
+                    var rotation = Quaternion.identity;
+                    rotation *= NifUtils.NifEulerAnglesToUnityQuaternion(
+                        new Vector3(cellRecord.CellLightingInfo.DirectionalRotationXY,
+                            cellRecord.CellLightingInfo.DirectionalRotationXY,
+                            cellRecord.CellLightingInfo.DirectionalRotationZ));
+                    directionalLight.transform.rotation = rotation;
+                }
+                else
+                {
+                    directionalLight.enabled = false;
+                }
+            }
+
+            //Inherit fog far distance
+            if ((cellRecord.CellLightingInfo.InheritFlags & 0x0010) != 0 && template != null)
+            {
+                if (template.LightingData.FogFar > 0)
+                {
+                    RenderSettings.fog = true;
+                    RenderSettings.fogMode = FogMode.Linear;
+                    RenderSettings.fogEndDistance = template.LightingData.FogFar / Convert.meterInMWUnits;
+                }
+                else
+                {
+                    RenderSettings.fog = false;
+                }
+            }
+            else
+            {
+                if (cellRecord.CellLightingInfo.FogFar > 0)
+                {
+                    RenderSettings.fog = true;
+                    RenderSettings.fogMode = FogMode.Linear;
+                    RenderSettings.fogEndDistance = cellRecord.CellLightingInfo.FogFar / Convert.meterInMWUnits;
+                }
+                else
+                {
+                    RenderSettings.fog = false;
+                }
+            }
+
+            //Inherit fog near distance
+            if (RenderSettings.fog && (cellRecord.CellLightingInfo.InheritFlags & 0x0008) != 0 && template != null)
+            {
+                RenderSettings.fogStartDistance = template.LightingData.FogNear / Convert.meterInMWUnits;
+            }
+            else if (RenderSettings.fog)
+            {
+                RenderSettings.fogStartDistance = cellRecord.CellLightingInfo.FogNear / Convert.meterInMWUnits;
+            }
+
+            //Inherit fog color
+            if (RenderSettings.fog && (cellRecord.CellLightingInfo.InheritFlags & 0x0004) != 0 && template != null)
+            {
+                RenderSettings.fogColor = new Color32(template.LightingData.FogNearColor[0],
+                    template.LightingData.FogNearColor[1], template.LightingData.FogNearColor[2], 255);
+            }
+            else if (RenderSettings.fog)
+            {
+                RenderSettings.fogColor = new Color32(cellRecord.CellLightingInfo.FogNearColor[0],
+                    cellRecord.CellLightingInfo.FogNearColor[1], cellRecord.CellLightingInfo.FogNearColor[2], 255);
+            }
         }
 
         private IEnumerator InstantiateCellReferences(Group referencesGroup, GameObject parent)
@@ -134,7 +248,8 @@ namespace Engine
             return modelObject;
         }
 
-        private GameObject InstantiateLightAtPositionAndRotation(REFR refr, LIGH lightRecord, float[] position,
+        private GameObject InstantiateLightAtPositionAndRotation(REFR lightReference, LIGH lightRecord,
+            float[] position,
             float[] rotation,
             float scale, GameObject parent)
         {
@@ -144,7 +259,7 @@ namespace Engine
             if (modelObject == null)
                 modelObject = new GameObject(lightRecord.EditorID);
 
-            InstantiateLightOnGameObject(refr, lightRecord, modelObject);
+            InstantiateLightOnGameObject(lightReference, lightRecord, modelObject);
             ApplyPositionAndRotation(position, rotation, scale, parent, modelObject);
 
             return modelObject;
@@ -152,34 +267,38 @@ namespace Engine
 
         private void InstantiateLightOnGameObject(REFR reference, LIGH lightRecord, GameObject gameObject)
         {
-            if (gameObject != null)
+            if (gameObject == null) return;
+            var light = gameObject.AddComponent<Light>();
+            //For some interesting reason the actual radius shown in CK is Base light radius + XRDS value of refr
+            light.range = (lightRecord.Radius + reference.Radius) / Convert.meterInMWUnits;
+            light.color = new Color32(lightRecord.ColorRGBA[0], lightRecord.ColorRGBA[1], lightRecord.ColorRGBA[2],
+                255);
+            //Intensity in Unity != intensity in Skyrim
+            light.intensity = 2 * (lightRecord.Fade + reference.FadeOffset);
+            if ((lightRecord.Flags & 0x0400) != 0)
             {
-                var light = gameObject.AddComponent<Light>();
-                //For some interesting reason the actual radius shown in CK is Base light radius + XRDS value of refr
-                light.range = (lightRecord.Radius + reference.Radius) / Convert.meterInMWUnits;
-                light.color = new Color32(lightRecord.ColorRGBA[0], lightRecord.ColorRGBA[1], lightRecord.ColorRGBA[2],
-                    255);
-                //Intensity in Unity != intensity in Skyrim
-                light.intensity = 3 * (lightRecord.Fade + reference.FadeOffset);
+                light.type = LightType.Spot;
+            }
+            else if ((lightRecord.Flags & 0x0800) == 0 && (lightRecord.Flags & 0x1000) == 0)
+            {
+                light.shadows = LightShadows.None;
             }
         }
 
         private static void ApplyPositionAndRotation(float[] position, float[] rotation, float scale, GameObject parent,
             GameObject modelObject)
         {
-            if (modelObject != null)
+            if (modelObject == null) return;
+            if (scale != 0f)
             {
-                if (scale != 0f)
-                {
-                    modelObject.transform.localScale = Vector3.one * scale;
-                }
-
-                modelObject.transform.position +=
-                    NifUtils.NifPointToUnityPoint(new Vector3(position[0], position[1], position[2]));
-                modelObject.transform.rotation *=
-                    NifUtils.NifEulerAnglesToUnityQuaternion(new Vector3(rotation[0], rotation[1], rotation[2]));
-                modelObject.transform.parent = parent.transform;
+                modelObject.transform.localScale = Vector3.one * scale;
             }
+
+            modelObject.transform.position +=
+                NifUtils.NifPointToUnityPoint(new Vector3(position[0], position[1], position[2]));
+            modelObject.transform.rotation *=
+                NifUtils.NifEulerAnglesToUnityQuaternion(new Vector3(rotation[0], rotation[1], rotation[2]));
+            modelObject.transform.parent = parent.transform;
         }
 
         public void DestroyAllCells()
