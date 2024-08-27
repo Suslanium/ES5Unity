a994c2988bbfe611886b3173d90089bfaae5d324
Suslanium <zaharov22m@gmail.com>
2024-08-28 03:08:57 +0700

    Logger fix

--- Assets/Scripts/Engine/Core/Logger.cs
+++ Assets/Scripts/Engine/Core/Logger.cs
@@ -2,21 +2,25 @@
 {
     public static class Logger
     {
-        #if (DEVELOPMENT_BUILD || UNITY_EDITOR)
         public static void Log(object message)
         {
+#if (DEVELOPMENT_BUILD || UNITY_EDITOR)
             UnityEngine.Debug.Log(message);
+#endif
         }
         
         public static void LogWarning(string message)
         {
+#if (DEVELOPMENT_BUILD || UNITY_EDITOR)
             UnityEngine.Debug.LogWarning(message);
+#endif
         }
         
         public static void LogError(string message)
         {
+#if (DEVELOPMENT_BUILD || UNITY_EDITOR)
             UnityEngine.Debug.LogError(message);
+#endif
         }
-        #endif
     }
 }
